6a5934537f8af00cacaae97c1dabc227415615d3
rishi-live <rishi.uemk9@gmail.com>
2021-01-16 23:25:17 +0530

    component update

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from "react";
+import {
+  FromGroup,
+  Input,
+  Button,
+  Form,
+  FormGroup,
+  InputGroup,
+  InputGroupAddon,
+} from "reactstrap";
+import { v4 } from "uuid";
+import { TodoContext } from "../Context/TodoContext";
+import { ADD_TODO } from "../Context/action.types";
+
+const TodoForm = () => {
+  const [todoString, setTodoString] = useState("");
+  const { dispatch } = useContext(TodoContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoString === "empty") {
+      return alert("Please enter a Todo");
+    }
+
+    const todo = {
+      todoString,
+      id: v4(),
+    };
+    dispatch({
+      type: ADD_TODO,
+      payload: todo,
+    });
+
+    setTodoString("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <InputGroup>
+          <Input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Your next Todo"
+            value={todoString}
+            onChange={(e) => setTodoString(e.target.value)}
+          />
+          <InputGroupAddon addonType="prepend">
+            <Button
+              color="warning"
+              //TODO: onclick
+            >
+              Add
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default TodoForm;
